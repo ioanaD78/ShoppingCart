7882891f87ae672db2bd6ca9ad246d33c3081112
ioanaD78 <ioana.dan.bv@gmail.com>
2021-11-27 22:05:38 +0200

    add to cart button working + some pages

--- products.js
+++ products.js
@@ -7,29 +7,56 @@ fetch('discs.json')
         });
     });
 
-let rowDiv = document.querySelector(".row");
 
 function localSave(discs) {
     localStorage.setItem("vinyls", JSON.stringify(discs));
 }
 
 const templateCard = document.querySelector(".templateCard");
-
 function createData(discProd) {
+    // cloning the product HTML template and assigning it to displayProd
     const displayProd = templateCard.cloneNode(true);
+
+    //removing the template from the page
     templateCard.remove();
+
+    //removing the "hidden" class from the cloned element so it becomes visible
     displayProd.classList.remove("templateCard");
 
     console.log(discProd);
 
+    //discProd - the disc info we receive from local storage
+    //inserting the info into HTML
     displayProd.querySelector(".mockImg").src = discProd.image;
     displayProd.querySelector(".mockTitle").innerHTML = discProd.title;
     displayProd.querySelector(".mockArtist").innerHTML = discProd.artist;
     displayProd.querySelector(".mockCondition").innerHTML = discProd.condition;
     displayProd.querySelector(".mockPrice").innerHTML = "$" + discProd.price;
 
+    //appending the new created product cards to the "products" container
     products.appendChild(displayProd);
 
+    //displaying the button for every product
+    //and adding addToCart function for every button
     displayProd.querySelector("mockButton");
+    displayProd.addEventListener("click", function () {
+        console.log(discProd.id);
+        addToCart(discProd.id);
+    })
+
 }
 
+//creating an empty array to hold each added object
+const cart = [];
+function addToCart(discId) {
+    let disc = vinyls.filter(disc => disc.id == discId);
+    // return an array with a single object inside
+    disc = disc[0];
+    // if produt not in cart, add
+    if (!cart.includes(disc)) {
+        cart.push(disc);
+    }
+
+    //saving to local storage
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
